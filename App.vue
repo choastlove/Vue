7d3881a5c953704d1f5d7a3b509aeb6d63491b84
choastlove <2577921390@qq.com>
2016-12-03 10:40:02 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,20 @@
+// App.vue
+<template>
+    <div class="box" v-text="note"></div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            note: '这是一个组件的html模板！'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.box {
+    color: #000;
+}
+</style>
